d945a98db0ada530f1011f0a16213f9716866af9
Saket045 <saketnigam7@gmail.com>
2024-08-01 10:23:36 +0530

    "follow users and reomver sugegsted users"

--- frontend/vite-project/src/components/common/RightPanel.jsx
+++ frontend/vite-project/src/components/common/RightPanel.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeleton/RightPanelSkeleton";
 import { useQuery } from "@tanstack/react-query";
-
+import useFollow from "../../hooks/useFollow";
+import LoadingSpinner from "./LoadingSpinner";
 const RightPanel = () => {
 	const {data:suggestedUsers,isLoading}=useQuery({queryKey:["suggestedUsers"],
 		queryFn:async()=>{
@@ -15,7 +16,9 @@ const RightPanel = () => {
 				throw new Error(error);
 			}
 		}
-	})
+	});
+	const {follow,isPending}=useFollow();
+
  if(suggestedUsers?.length===0) return <div className="md:w-64 w-0"></div>
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -55,9 +58,10 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {e.preventDefault();
+											follow(user._id);}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size="sm"/>:"Follow"}
 									</button>
 								</div>
 							</Link>
