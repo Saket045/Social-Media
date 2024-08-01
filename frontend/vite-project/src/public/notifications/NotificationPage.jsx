a933d6d24dacf8614a9507ad98d061d60eeb4a9e
Saket045 <saketnigam7@gmail.com>
2024-08-01 16:00:19 +0530

    "notificationpage"

--- frontend/vite-project/src/public/notifications/NotificationPage.jsx
+++ frontend/vite-project/src/public/notifications/NotificationPage.jsx
@@ -1,37 +1,52 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
+   const queryClient=useQueryClient();
+	const {data:notifications,isLoading}=useQuery({
+		queryKey: ["notifications"],
+		queryFn: async () => {
+			try{
+			const response = await fetch("/api/notifications");
+			const data=await response.json();
+			if(!response.ok)
+				throw new Error(data.error);
+			return data;
+		} catch(error){
+			toast.error(error);
+		}
+		}
+	})
+	const {mutate:deleteNotifications}=useMutation({
+		mutationFn:async()=>{
+			try{
+                const response = await fetch("/api/notifications",
+					{
+						method:"DELETE",
+					}
+				);
+			const data=await response.json();
+			if(!response.ok)
+				throw new Error(data.error);
+			return data;
+			}
+			catch(error){
+				toast.error(error);
+			}
 		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
+		onSuccess:()=>{
+             queryClient.invalidateQueries({queryKey:['notifications']})
 		},
-	];
-
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
-
+		onError:()=>{
+			toast.error("Failed to delete notifications")
+		}
+	})
 	return (
 		<>
 			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
