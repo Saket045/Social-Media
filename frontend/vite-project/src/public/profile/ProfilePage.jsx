158fe86e96700e5485825b7d2f3cdb2f5238f28b
Saket045 <saketnigam7@gmail.com>
2024-08-03 09:26:08 +0530

    "profile"

--- frontend/vite-project/src/public/profile/ProfilePage.jsx
+++ frontend/vite-project/src/public/profile/ProfilePage.jsx
@@ -161,12 +161,12 @@ useEffect(()=>{
 											<>
 												<FaLink className='w-3 h-3 text-slate-500' />
 												<a
-													href='https://youtube.com/@asaprogrammer_'
+													href={user?.link}
 													target='_blank'
 													rel='noreferrer'
 													className='text-sm text-blue-500 hover:underline'
 												>
-													youtube.com/@asaprogrammer_
+													{user?.link}
 												</a>
 											</>
 										</div>
