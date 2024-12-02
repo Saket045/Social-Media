c8fcd3ecf6f29e986a80b4c922615f5483ac6576
CDCSaket <saketnigam@curiousdevelopers.in>
2024-12-02 14:11:12 +0530

    "to deploy"

--- frontend/vite-project/src/public/auth/signup/SignUpPage.jsx
+++ frontend/vite-project/src/public/auth/signup/SignUpPage.jsx
@@ -56,12 +56,10 @@ const SignUpPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				{/* <XSvg className=' lg:w-2/3 fill-white' /> */}
-			</div>
+			
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					{/* <XSvg className='w-24 lg:hidden fill-white' /> */}
+				
 					<h1 className='text-4xl font-extrabold text-white'>Join today.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
